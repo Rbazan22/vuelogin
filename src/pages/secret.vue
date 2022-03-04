173511d8405ad82d5ca1160eca7c904f08ba9090
Rbazan22 <rbazan22@stu.southtexascollege.edu>
2022-03-03 18:45:56 -0600

    Altered Secret Page

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,3 +1,9 @@
 <template>
-<p>Welcome to the Secret Page. Only the best of the best can access this page!</p>
+<div class="flex flex-col items-center justify-center min-h-screen-nonav">
+<h2 class="text-6xl py-4 tracking-tighter font-extralight">Secret Page</h2>
+<h1 class="text-red-600">Welcome to the Secret Page. Only the best of the best can access this page!</h1>
+
+<img class="rounded-lg py-4" src="../assets/blackhat.jpg" alt="Secret BG">
+</div>
+
 </template>

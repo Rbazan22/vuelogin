a764e052728b662479981b80ff94b4ab1c08529c
Rbazan22 <rbazan22@stu.southtexascollege.edu>
2022-03-04 23:56:51 -0600

    Added Basic Validation

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,24 @@
 <script setup>
-import {ref} from "vue";
 import {useRouter} from "vue-router";
+import {useForm, useField} from "vee-validate";
+import * as yup from "yup";
+
+const schema = yup.object({
+  username: yup.string().required().email().label("Email"),
+  password: yup.string().required().min(8).label("Password"),
+});
+
+useForm ({
+  validationSchema: schema,
+});
+
+const { value: username, errorMessage: emailError } = useField("username");
+const { value: password, errorMessage: passwordError } = useField("password");
+
 import useAuth from "../composable/useAuth";
 import useError from "../composable/useError";
 
 const { isAuthenticated, login, signup, googleLogin } = useAuth();
-
-const username = ref("");
-const password = ref("");
 
 const router = useRouter();
 
@@ -44,12 +55,28 @@ const { ready, start } = useTimeout(3000, { controls: true });
     <div class="flex items-center justify-center bg-gray-200 overflow-hidden rounded-lg shadow-2xl">
     <img class="h-64" src="../assets/bglogin.png" alt="Hello BG" />
     <form @submit.prevent="logginIn" class="flex flex-col p-4 space-y-4">
-        <input type="text" class="p-2 border-2 rounded-lg" placeholder="Username" v-model="username" />
-        <input type="password" class="p-2 border-2 rounded-lg" placeholder="Password" v-model="password" />
+        <input 
+        name="username" 
+        type="text" 
+        class="p-2 border-2 rounded-lg" placeholder="Enter Email" 
+        v-model="username" 
+        />
+        <span class="text-xs text-center text-red-500">{{emailError}}</span>
+        <input 
+        name="password"
+        type="password" 
+        class="p-2 border-2 rounded-lg" placeholder="Enter Password" 
+        v-model="password" 
+        />
+        <span class="text-xs text-center text-red-500">{{passwordError}}</span>
+
         <div class="flex space-x-2">
-        <button type="submit" @submit.prevent="logginIn" class="w-1/2 py-2 text-green-200 bg-green-600 rounded-lg" >Login</button>   
+        <button 
+        type="submit" 
+        @submit.prevent="logginIn" 
+        class="w-1/2 py-2 text-green-200 bg-green-600 rounded-lg hover:bg-green-700" >Login</button>   
         
-        <button @click="signingUp" class="w-1/2 py-2 text-purple-200 bg-purple-600 rounded-lg" >Sign Up</button>   
+        <button @click="signingUp" class="w-1/2 py-2 text-purple-200 bg-purple-600 rounded-lg hover:bg-purple-700" >Sign Up</button>   
         </div>
     <button @click="google"
     class="flex justify-center py-2 bg-white rounded-lg hover:bg-gray-300"><img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt=""></button>

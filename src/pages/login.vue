f58f987e2de00694fdf914e200bc70656224927a
Rbazan22 <rbazan22@stu.southtexascollege.edu>
2022-04-20 18:32:06 -0500

    Create chat layout

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {useRouter} from "vue-router";
-import {useForm, useField} from "vee-validate";
+import { useRouter } from "vue-router";
+import { useForm, useField } from "vee-validate";
 import * as yup from "yup";
 
 const schema = yup.object({
@@ -8,7 +8,7 @@ const schema = yup.object({
   password: yup.string().required().min(8).label("Password"),
 });
 
-useForm ({
+useForm({
   validationSchema: schema,
 });
 
@@ -34,59 +34,80 @@ const signingUp = async () => {
 const google = async () => {
   await googleLogin();
   goToHome();
-}
+};
 
 const goToHome = () => {
   if (isAuthenticated.value) {
-      router.push("/");
-    } else {
-      setError("Invalid username or password");
-      start();
-    }
+    router.push("/");
+  } else {
+    setError("Invalid username or password");
+    start();
+  }
 };
 
-const {error, setError} = useError();
+const { error, setError } = useError();
 import { useTimeout, promiseTimeout } from "@vueuse/core";
 const { ready, start } = useTimeout(3000, { controls: true });
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen-nonav">
-    <div class="flex items-center justify-center bg-gray-200 overflow-hidden rounded-lg shadow-2xl">
-    <img class="h-64" src="../assets/bglogin.png" alt="Hello BG" />
-    <form @submit.prevent="logginIn" class="flex flex-col p-4 space-y-4">
-        <input 
-        name="username" 
-        type="text" 
-        class="p-2 border-2 rounded-lg" placeholder="Enter Email" 
-        v-model="username" 
+    <div
+      class="flex items-center justify-center bg-gray-200 overflow-hidden rounded-lg shadow-2xl"
+    >
+      <img class="h-64" src="../assets/bglogin.png" alt="Hello BG" />
+      <form @submit.prevent="logginIn" class="flex flex-col p-4 space-y-4">
+        <input
+          name="username"
+          type="text"
+          class="p-2 border-2 rounded-lg"
+          placeholder="Enter Email"
+          v-model="username"
         />
-        <span class="text-xs text-center text-red-500">{{emailError}}</span>
-        <input 
-        name="password"
-        type="password" 
-        class="p-2 border-2 rounded-lg" placeholder="Enter Password" 
-        v-model="password" 
+        <span class="text-xs text-center text-red-500">{{ emailError }}</span>
+        <input
+          name="password"
+          type="password"
+          class="p-2 border-2 rounded-lg"
+          placeholder="Enter Password"
+          v-model="password"
         />
-        <span class="text-xs text-center text-red-500">{{passwordError}}</span>
+        <span class="text-xs text-center text-red-500">{{
+          passwordError
+        }}</span>
 
         <div class="flex space-x-2">
-        <button 
-        type="submit" 
-        @submit.prevent="logginIn" 
-        class="w-1/2 py-2 text-green-200 bg-green-600 rounded-lg hover:bg-green-700" >Login</button>   
-        
-        <button @click="signingUp" class="w-1/2 py-2 text-purple-200 bg-purple-600 rounded-lg hover:bg-purple-700" >Sign Up</button>   
+          <button
+            type="submit"
+            @submit.prevent="logginIn"
+            class="w-1/2 py-2 text-green-200 bg-green-600 rounded-lg hover:bg-green-700"
+          >
+            Login
+          </button>
+
+          <button
+            @click="signingUp"
+            class="w-1/2 py-2 text-purple-200 bg-purple-600 rounded-lg hover:bg-purple-700"
+          >
+            Sign Up
+          </button>
         </div>
-    <button @click="google"
-    class="flex justify-center py-2 bg-white rounded-lg hover:bg-gray-300"><img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt=""></button>
-    </form>
+        <button
+          @click="google"
+          class="flex justify-center py-2 bg-white rounded-lg hover:bg-gray-300"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            alt=""
+          />
+        </button>
+      </form>
     </div>
-    <div 
-    v-if="!ready && error"
-    class="absolute w=1/3 p-4 bg-red-300 text-center rounded-lg text-red-800 bottom-2 right-2"
+    <div
+      v-if="!ready && error"
+      class="absolute w=1/3 p-4 bg-red-300 text-center rounded-lg text-red-800 bottom-2 right-2"
     >
-    {{error}}
+      {{ error }}
     </div>
   </div>
 </template>
--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,9 +1,23 @@
 <template>
-<div class="flex flex-col items-center justify-center min-h-screen-nonav">
-<h2 class="text-6xl py-4 tracking-tighter font-extralight">Secret Page</h2>
-<h1 class="text-red-600">Welcome to the Secret Page. Only the best of the best can access this page!</h1>
-
-<img class="rounded-lg py-4" src="../assets/blackhat.jpg" alt="Secret BG">
-</div>
-
+  <h1 class="text-6xl font-thin tracking-tighter text-center mt-8">
+    Cool Chat
+  </h1>
+  <div
+    class="min-h-[400px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between"
+  >
+    <ul class="p-4 space-y-4">
+      <li v-for="n in 5">
+        <div class="flex justify-between bg-gray-200 px-4 py-2 rounded-lg">
+          <span>Message itself</span><span>by Author Name</span>
+        </div>
+      </li>
+    </ul>
+    <div>
+      <input
+        class="w-full p-4 rounded-lg focus:outline-none focus:bg-yellow-200"
+        type="text"
+        placeholder="Type a message..."
+      />
+    </div>
+  </div>
 </template>

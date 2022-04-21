2e4a4fcdd027d8adbe5b89837c891b00cea766dd
Rbazan22 <rbazan22@stu.southtexascollege.edu>
2022-04-20 19:27:29 -0500

    Adjusted Colors and Secret Title

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,52 +1,71 @@
 <script setup>
-import { useRouter } from 'vue-router';
-import useAuth from '../composable/useAuth';
+import { useRouter } from "vue-router";
+import useAuth from "../composable/useAuth";
 
-const {isAuthenticated, logout} = useAuth();
+const { isAuthenticated, logout } = useAuth();
 
 const router = useRouter();
 
 const loggingOut = () => {
-    logout();
-    router.push("/");
+  logout();
+  router.push("/");
 };
 </script>
 
 <template>
-<div class="bg-red-800 text-red-200">
+  <div class="bg-blue-800 text-blue-200">
     <div class="container mx-auto flex items-center justify-between">
-        <h1 class="px-2 tracking-tighter text-3xl font-thin">Vue<span class="font-normal">RealAuth</span>
-        </h1>
-        <nav>
-            <ul class="flex space-x-4">
-                <router-link to="/">
-                <li class="py-8 px-4 hover:cursor-pointer hover:bg-red-500 hover:text-red-800 hover:animate-pulse"> Home
-                </li>
-                </router-link>
+      <h1 class="px-2 tracking-tighter text-3xl font-thin">
+        Vue<span class="font-normal">RealAuth</span>
+      </h1>
+      <nav>
+        <ul class="flex space-x-4">
+          <router-link to="/">
+            <li
+              class="py-8 px-4 hover:cursor-pointer hover:bg-blue-500 hover:text-blue-800 hover:animate-pulse"
+            >
+              Home
+            </li>
+          </router-link>
 
-                <router-link :to = "{path: '/about', name: 'About'}">
-                <li class="py-8 px-4 hover:cursor-pointer hover:bg-red-500 hover:text-red-800 hover:animate-pulse">About
-                </li>
-                </router-link>
+          <router-link :to="{ path: '/about', name: 'About' }">
+            <li
+              class="py-8 px-4 hover:cursor-pointer hover:bg-blue-500 hover:text-blue-800 hover:animate-pulse"
+            >
+              About
+            </li>
+          </router-link>
 
-                <router-link v-if="!isAuthenticated" :to = "{path: '/login', name: 'Login'}">
-                <li class="py-8 px-4 hover:cursor-pointer hover:bg-red-500 hover:text-red-800 hover:animate-pulse">Login
-                </li>
-                </router-link>
+          <router-link
+            v-if="!isAuthenticated"
+            :to="{ path: '/login', name: 'Login' }"
+          >
+            <li
+              class="py-8 px-4 hover:cursor-pointer hover:bg-blue-500 hover:text-blue-800 hover:animate-pulse"
+            >
+              Login
+            </li>
+          </router-link>
 
-                <div v-else class="flex">
-                <router-link :to = "{ name: 'Secret' }">
-                <li class="py-8 px-4 hover:cursor-pointer hover:bg-red-500 hover:text-red-800 hover:animate-pulse">Secret
-                </li>
-                </router-link>
+          <div v-else class="flex">
+            <router-link :to="{ name: 'Secret' }">
+              <li
+                class="py-8 px-4 hover:cursor-pointer hover:bg-blue-500 hover:text-blue-800 hover:animate-pulse"
+              >
+                Secret
+              </li>
+            </router-link>
 
-                <button @click="loggingOut">
-                <li class="py-8 px-4 hover:cursor-pointer hover:bg-red-500 hover:text-red-800 hover:animate-pulse">Logout
-                </li>
-                </button>
-                </div>
-            </ul>
-        </nav>
+            <button @click="loggingOut">
+              <li
+                class="py-8 px-4 hover:cursor-pointer hover:bg-blue-500 hover:text-blue-800 hover:animate-pulse"
+              >
+                Logout
+              </li>
+            </button>
+          </div>
+        </ul>
+      </nav>
     </div>
-</div>
+  </div>
 </template>
--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -20,7 +20,7 @@ onUnmounted(() => {
 
 <template>
   <h1 class="text-6xl font-thin tracking-tighter text-center mt-8">
-    Cool Chat
+    Spectacular Chat
   </h1>
   <div
     class="min-h-[400px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between"
@@ -29,7 +29,7 @@ onUnmounted(() => {
       <li v-for="message in messages" :key="message.id">
         <div
           class="flex justify-between px-4 py-2 rounded-lg"
-          :class="user === message.author ? 'bg-yellow-200' : 'bg-gray-200'"
+          :class="user === message.author ? 'bg-green-200' : 'bg-gray-200'"
         >
           <span>{{ message.text }}</span
           ><span>by {{ message.author }}</span>
@@ -38,7 +38,7 @@ onUnmounted(() => {
     </ul>
     <div>
       <input
-        class="w-full p-4 rounded-lg focus:outline-none focus:bg-yellow-200"
+        class="w-full p-4 rounded-lg focus:outline-none focus:bg-green-200"
         type="text"
         placeholder="Type a message..."
         v-model="newMessage"
